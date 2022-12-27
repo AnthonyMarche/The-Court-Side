72368e7dfe83e8f5affa17b374c1fe26238ad0b7
yazidham <79407250+yazidham@users.noreply.github.com>
2022-12-27 19:25:37 +0100

    wip

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Video;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -12,5 +13,13 @@ class HomeController extends AbstractController
     public function index(): Response
     {
         return $this->render('home/index.html.twig');
+    }
+
+    #[Route('/watch/{id}', name: 'app_watch', methods: ['GET'])]
+    public function watch(Video $video): Response
+    {
+        return $this->render('home/watch.html.twig', [
+            'video' => $video,
+        ]);
     }
 }
