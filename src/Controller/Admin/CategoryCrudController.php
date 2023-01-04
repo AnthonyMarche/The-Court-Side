58d4ee3915cc36999062d554386e8f3f84c530c7
AnthonyMarche <marche.anthony72@gmail.com>
2023-01-04 16:06:11 +0100

    add a new category

--- src/Controller/Admin/CategoryCrudController.php
+++ src/Controller/Admin/CategoryCrudController.php
@@ -3,13 +3,23 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Category;
+use DateTime;
+use Doctrine\ORM\EntityManagerInterface;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\DateTimeField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use Symfony\Component\String\Slugger\SluggerInterface;
 
 class CategoryCrudController extends AbstractCrudController
 {
+    private SluggerInterface $slugger;
+
+    public function __construct(SluggerInterface $slugger)
+    {
+        $this->slugger = $slugger;
+    }
+
     public static function getEntityFqcn(): string
     {
         return Category::class;
@@ -33,5 +43,26 @@ class CategoryCrudController extends AbstractCrudController
                 ->setFormat('dd/MM/YYYY')
                 ->onlyOnIndex(),
         ];
+    }
+
+    public function persistEntity(EntityManagerInterface $entityManager, $entityInstance): void
+    {
+        if (!$entityInstance instanceof Category) {
+            return;
+        }
+
+        $slug = $this->slugger->slug($entityInstance->getName());
+        $entityInstance->setSlug($slug);
+
+        $entityManager->persist($entityInstance);
+        $entityManager->flush();
+    }
+
+    public function createEntity(string $entityFqcn): Category
+    {
+        $category = new Category();
+        $category->setCreatedAt(new DateTime());
+
+        return $category;
     }
 }
