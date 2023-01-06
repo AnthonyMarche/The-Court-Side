b014f15db1d32a55ab0f73b0e8345e107f1370a8
AnthonyMarche <marche.anthony72@gmail.com>
2023-01-06 11:16:41 +0100

    crud video

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -20,10 +20,8 @@ class DashboardController extends AbstractDashboardController
     private UserRepository $userRepository;
     private VideoRepository $videoRepository;
 
-    public function __construct(
-        UserRepository $userRepository,
-        VideoRepository $videoRepository,
-    ) {
+    public function __construct(UserRepository $userRepository, VideoRepository $videoRepository)
+    {
         $this->userRepository = $userRepository;
         $this->videoRepository = $videoRepository;
     }
@@ -31,23 +29,6 @@ class DashboardController extends AbstractDashboardController
     #[Route('/admin', name: 'admin')]
     public function index(): Response
     {
-//        return parent::index();
-
-        // Option 1. You can make your dashboard redirect to some common page of your backend
-        //
-        // $adminUrlGenerator = $this->container->get(AdminUrlGenerator::class);
-        // return $this->redirect($adminUrlGenerator->setController(OneOfYourCrudController::class)->generateUrl());
-
-        // Option 2. You can make your dashboard redirect to different pages depending on the user
-        //
-        // if ('jane' === $this->getUser()->getUsername()) {
-        //     return $this->redirect('...');
-        // }
-
-        // Option 3. You can render some custom template to display a proper dashboard with widgets, etc.
-        // (tip: it's easier if your template extends from @EasyAdmin/page/content.html.twig)
-        //
-
         // --- GENERATION DES STATISTIQUES -- //
 
         // récupère tous les utilisateurs (via le UserRepository)
--- src/Controller/Admin/VideoCrudController.php
+++ src/Controller/Admin/VideoCrudController.php
@@ -3,16 +3,30 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Video;
+use DateTime;
+use Doctrine\ORM\EntityManagerInterface;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\BooleanField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\DateTimeField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ImageField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextareaField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use Symfony\Component\String\Slugger\SluggerInterface;
 
 class VideoCrudController extends AbstractCrudController
 {
+    private const PATHVIDEO = 'uploads/videos';
+    private const PATHTEASER = 'uploads/teasers';
+
+    private SluggerInterface $slugger;
+
+    public function __construct(SluggerInterface $slugger)
+    {
+        $this->slugger = $slugger;
+    }
+
     public static function getEntityFqcn(): string
     {
         return Video::class;
@@ -23,11 +37,8 @@ class VideoCrudController extends AbstractCrudController
         return $crud
             ->setEntityLabelInSingular('vidéo')
             ->setEntityLabelInPlural('vidéos')
-
             ->setPageTitle('index', ' Liste des %entity_label_plural%')
-
             ->setSearchFields(['title', 'description', 'category.name'])
-
             ->setDefaultSort(['createdAt' => 'DESC']);
     }
 
@@ -43,8 +54,86 @@ class VideoCrudController extends AbstractCrudController
 
             AssociationField::new('category', 'Catégorie'),
 
+            AssociationField::new('tag', 'Tag')
+                ->onlyOnForms(),
+
+            ImageField::new('url', 'Fichier vidéo')
+                ->onlyWhenCreating()
+                ->setBasePath(self::PATHVIDEO)
+                ->setUploadDir('public/uploads/videos')
+                ->setUploadedFileNamePattern(self::PATHVIDEO . '/[slug]-[timestamp].[extension]')
+                ->setHelp('Fichiers .avi, .mp4, .ogg and .wbm'),
+
+            ImageField::new('teaser', 'Ajouter un teaser')
+                ->onlyWhenCreating()
+                ->setBasePath(self::PATHTEASER)
+                ->setUploadDir('public/uploads/teasers')
+                ->setUploadedFileNamePattern(self::PATHTEASER . '/teaser-[slug]-[timestamp].[extension]'),
+
             DateTimeField::new('createdAt', 'Créé le')
+                ->onlyOnIndex()
                 ->setFormat('dd/MM/YYYY'),
         ];
+    }
+
+    public function persistEntity(EntityManagerInterface $entityManager, $entityInstance): void
+    {
+        if (!$entityInstance instanceof Video) {
+            return;
+        }
+
+        /** @var \App\Entity\User */
+        $user = $this->getUser();
+        $entityInstance->setUser($user);
+
+        $slug = $this->slugger->slug($entityInstance->getTitle());
+        $entityInstance->setSlug($slug);
+
+        $entityManager->persist($entityInstance);
+        $entityManager->flush();
+    }
+
+    public function createEntity(string $entityFqcn): Video
+    {
+        $video = new Video();
+
+        $video->setNumberOfView(0);
+        $video->setCreatedAt(new DateTime());
+
+        return $video;
+    }
+
+    public function updateEntity(EntityManagerInterface $entityManager, $entityInstance): void
+    {
+
+        if (!$entityInstance instanceof Video) {
+            return;
+        }
+
+        /** @var \App\Entity\User */
+        $user = $this->getUser();
+        $entityInstance->setUser($user);
+
+        $slug = $this->slugger->slug($entityInstance->getTitle());
+        $entityInstance->setSlug($slug);
+
+        $entityInstance->setUpdatedAt(new DateTime());
+
+        $entityManager->persist($entityInstance);
+        $entityManager->flush();
+    }
+
+    public function deleteEntity(EntityManagerInterface $entityManager, $entityInstance): void
+    {
+        $video = $entityInstance->getUrl();
+        $teaser = $entityInstance->getTeaser();
+
+        unlink($video);
+        if ($teaser != null) {
+            unlink($video);
+        }
+
+        $entityManager->remove($entityInstance);
+        $entityManager->flush();
     }
 }
