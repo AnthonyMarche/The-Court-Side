615a023bf36e8befa1ecb2dcfd9d18e111875a80
yazidham <79407250+yazidham@users.noreply.github.com>
2023-01-09 12:00:38 +0100

    added a sorting info in the filter part for more user friendliness

--- assets/js/filter.js
+++ assets/js/filter.js
@@ -4,10 +4,33 @@ if (document.querySelector('.js-content')) {
     const viewsFilter = document.getElementById('viewsFilter')
     const content = document.querySelector('.js-content')
     const loadingIcon = document.querySelector('.loading-container')
+    const sortingLikes = document.querySelector('.sorted-by-likes')
+    const sortingViews = document.querySelector('.sorted-by-views')
+    const sortingRecent = document.querySelector('.sorted-by-recent')
+    const delay = ms => new Promise(res => setTimeout(res, ms));
 
     dateFilter.addEventListener('click', applyFilter);
     likesFilter.addEventListener('click', applyFilter);
     viewsFilter.addEventListener('click', applyFilter);
+
+    viewsFilter.addEventListener('click', async function () {
+        await delay(1500);
+        sortingViews.classList.remove('disappearance');
+        sortingRecent.classList.add('disappearance');
+        sortingLikes.classList.add('disappearance');
+    });
+    likesFilter.addEventListener('click', async function () {
+        await delay(1500);
+        sortingViews.classList.add('disappearance');
+        sortingRecent.classList.add('disappearance');
+        sortingLikes.classList.remove('disappearance');
+    });
+    dateFilter.addEventListener('click', async function () {
+        await delay(1500);
+        sortingViews.classList.add('disappearance');
+        sortingRecent.classList.remove('disappearance');
+        sortingLikes.classList.add('disappearance');
+    });
 
     // eslint-disable-next-line no-inner-declarations
     async function applyFilter(event) {
@@ -25,7 +48,6 @@ if (document.querySelector('.js-content')) {
             content.innerHTML = data.content
 
             //fake loading
-            const delay = ms => new Promise(res => setTimeout(res, ms));
             loadingIcon.classList.remove('disappearance');
             loadingIcon.classList.add('apparition');
             await delay(1500);
@@ -34,6 +56,11 @@ if (document.querySelector('.js-content')) {
 
             history.replaceState({}, '', link)
         }
+    }
+
+    // eslint-disable-next-line no-inner-declarations
+    function sortingInfo() {
+
     }
 }
 
