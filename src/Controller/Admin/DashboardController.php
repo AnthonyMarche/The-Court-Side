4f686d7d28eb4080b1c2f1dd983e4b2275047680
AnthonyMarche <marche.anthony72@gmail.com>
2023-01-19 17:38:33 +0100

    fix dashboard

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -17,7 +17,6 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Translation\TranslatableMessage;
 
-/** @suppressWarnings(PHP.ExcessiveMethodLength) */
 class DashboardController extends AbstractDashboardController
 {
     private UserRepository $userRepository;
