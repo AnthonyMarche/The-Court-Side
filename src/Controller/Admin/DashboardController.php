f349e879ec8e0d87609b894ac4460f53e4672b9f
Bokassa jean le Grand <jean-le-grand.bokassa@wildcodeschool.com>
2023-01-19 12:26:07 +0100

    fix quality code in easy admin

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -17,6 +17,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Translation\TranslatableMessage;
 
+/** @suppressWarnings(PHP.ExcessiveMethodLength) */
 class DashboardController extends AbstractDashboardController
 {
     private UserRepository $userRepository;
@@ -71,6 +72,9 @@ class DashboardController extends AbstractDashboardController
             ->setTitle('THE COURT SIDE | Admin');
     }
 
+    /**
+     * @SuppressWarnings(PHPMD)
+     */
     public function configureMenuItems(): iterable
     {
         yield MenuItem::linkToDashboard(
