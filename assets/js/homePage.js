9450ab0fb1bc33de075003d79c09d89b19bb812f
yazidham <79407250+yazidham@users.noreply.github.com>
2023-01-27 14:24:19 +0100

    added the video if the teaser is not available and reworked on the video hover

--- assets/js/homePage.js
+++ assets/js/homePage.js
@@ -1,11 +1,35 @@
 // hover to play on videos
-const clip = document.querySelectorAll(".hover-to-play");
-for (let i = 0; i < clip.length; i++) {
-    clip[i].addEventListener("mouseenter", function (e) {
-        clip[i].play();
+const teaser = document.querySelectorAll(".hover-to-play-teaser");
+const video = document.querySelectorAll(".hover-to-play-video");
+const delay = ms => new Promise(res => setTimeout(res, ms));
+
+for (let i = 0; i < teaser.length; i++) {
+    teaser[i].addEventListener("mouseenter", async function (e) {
+        teaser[i].play();
     });
-    clip[i].addEventListener("mouseout", function (e) {
-        clip[i].pause();
+    teaser[i].addEventListener("mouseout", async function (e) {
+        teaser[i].currentTime = 0;
+        await delay(100);
+        teaser[i].pause();
     });
 }
 
+for (let i = 0; i < video.length; i++) {
+    let play;
+    video[i].onmouseover = async function () {
+        play = true;
+        while (play === true) {
+            video[i].play();
+            await delay(15000);
+            video[i].pause();
+            video[i].currentTime = 0;
+        }
+    }
+
+    video[i].onmouseout = async function () {
+        play = false;
+        video[i].currentTime = 0;
+        await delay(100);
+        video[i].pause();
+    }
+}
