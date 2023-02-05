f6dab112c4dd186ce4565ff6be85c08b4c713c2b
AnthonyMarche <marche.anthony72@gmail.com>
2023-02-05 21:17:20 +0100

    reformat newsletter js

--- assets/js/newsletter.js
+++ assets/js/newsletter.js
@@ -1,20 +1,20 @@
-if (document.getElementById('newsletter-body')) {
-    let newsletterBody = document.getElementById('newsletter-body');
-    let submitButton = document.getElementById('newsletter_submit');
-    let titleInput = document.getElementById('newsletter_title');
-    let contentInput = CKEDITOR.instances['newsletter_content'];
-    let loader = document.getElementById('adminLoader');
+let newsletterBody = document.getElementById('newsletter-body');
+let submitButton = document.getElementById('newsletter_submit');
+let titleInput = document.getElementById('newsletter_title');
+let contentInput = CKEDITOR.instances['newsletter_content'];
+let loader = document.getElementById('adminLoader');
 
+function updateButtonState() {
+    if (titleInput.value.length > 0 && contentInput.getData().length > 0) {
+        submitButton.removeAttribute("disabled");
+    } else {
+        submitButton.setAttribute("disabled", "disabled");
+    }
+}
+
+if (newsletterBody) {
     titleInput.addEventListener("input", updateButtonState);
     contentInput.on('change', updateButtonState);
-
-    function updateButtonState() {
-        if (titleInput.value.length > 0 && contentInput.getData().length > 0) {
-            submitButton.removeAttribute("disabled");
-        } else {
-            submitButton.setAttribute("disabled", "disabled");
-        }
-    }
 
     submitButton.addEventListener("click", async function () {
         loader.classList.remove('d-none');
