1fb29d8e01b3b282b57e7ae0ba31e0fac4310eb1
yazidham <79407250+yazidham@users.noreply.github.com>
2023-02-06 10:06:33 +0100

    fixed the about page style

--- src/Controller/AboutController.php
+++ src/Controller/AboutController.php
@@ -2,6 +2,9 @@
 
 namespace App\Controller;
 
+use App\Repository\LikeRepository;
+use App\Repository\UserRepository;
+use App\Repository\VideoRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
