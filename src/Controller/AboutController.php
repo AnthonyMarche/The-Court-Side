709561d8b04453eee3bc6b2acce694f4f684fd62
yazidham <79407250+yazidham@users.noreply.github.com>
2023-02-06 10:09:23 +0100

    removed the unused uses

--- src/Controller/AboutController.php
+++ src/Controller/AboutController.php
@@ -2,9 +2,6 @@
 
 namespace App\Controller;
 
-use App\Repository\LikeRepository;
-use App\Repository\UserRepository;
-use App\Repository\VideoRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
