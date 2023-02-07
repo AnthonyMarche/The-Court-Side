0036a74cd6e62d073ec83c4fb09726a594288661
AlexisBoucherie <alexis.boucherie@gmail.com>
2023-02-07 10:29:16 +0100

    prepare translations tags for user profile in views and controller

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -12,6 +12,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Translation\TranslatableMessage;
 
 #[Route('/user')]
 class UserController extends AbstractController
@@ -21,33 +22,6 @@ class UserController extends AbstractController
     public function __construct(UserPasswordHasherInterface $passwordHasher)
     {
         $this->passwordHasher = $passwordHasher;
-    }
-
-    #[Route('/', name: 'app_user_index', methods: ['GET'])]
-    public function index(UserRepository $userRepository): Response
-    {
-        return $this->render('user/index.html.twig', [
-            'users' => $userRepository->findAll(),
-        ]);
-    }
-
-    #[Route('/new', name: 'app_user_new', methods: ['GET', 'POST'])]
-    public function new(Request $request, UserRepository $userRepository): Response
-    {
-        $user = new User();
-        $form = $this->createForm(UserType::class, $user);
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $userRepository->save($user, true);
-
-            return $this->redirectToRoute('app_user_index', [], Response::HTTP_SEE_OTHER);
-        }
-
-        return $this->renderForm('user/new.html.twig', [
-            'user' => $user,
-            'form' => $form,
-        ]);
     }
 
     #[Route('/{id}', name: 'app_user_show', methods: ['GET'])]
@@ -100,7 +74,7 @@ class UserController extends AbstractController
                     $date = new DateTime('now');
                     $user->setUpdatedAt($date);
                     $userRepository->save($user, true);
-                    $this->addFlash('success', 'Votre profil à bien été mis à jour.');
+                    $this->addFlash('success', new TranslatableMessage('userprofile.edit-profile-flash'));
                     return $this->redirectToRoute('app_user_show', ['id' => $user->getId()]);
                 }
             } else {
@@ -108,7 +82,7 @@ class UserController extends AbstractController
                 $date = new DateTime('now');
                 $user->setUpdatedAt($date);
                 $userRepository->save($user, true);
-                $this->addFlash('success', 'Votre profil à bien été mis à jour.');
+                $this->addFlash('success', new TranslatableMessage('userprofile.edit-profile-flash'));
                 return $this->redirectToRoute('app_user_show', ['id' => $user->getId()]);
             }
         }
